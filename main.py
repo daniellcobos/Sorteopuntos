9ec2578a03e5843663b6bff404a15139c30506d8
unknown <daniel.leocobos@gmail.com>
2021-10-06 11:18:21 -0500

    mejoras esteticas y respuestas

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sorteo import mainSorteo
 
 app = Flask(__name__)
 
-@app.route('/form')
+@app.route('/')
 def form():
     #Pagina del formulario
     return render_template('Form.html')
@@ -16,6 +16,6 @@ def data():
     puntos3 = int(request.form['puntos3'])
     # Hace sorteo con los datos del formulario
     mainSorteo(ciudad,puntos1,puntos2,puntos3)
-    return ('Exito')
+    return render_template('res.html',ciudad = ciudad, puntos1 = puntos1, puntos2 = puntos2, puntos3 = puntos3)
 
 app.run(debug= True)
