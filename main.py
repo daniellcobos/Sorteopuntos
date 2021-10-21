7a4088feb3bc2bc9370d5ed6f30c98eb9e1d43b1
unknown <daniel.leocobos@gmail.com>
2021-10-20 21:55:35 -0500

    everythongs allrite

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from flask import Flask,request,render_template
 from sorteo import mainSorteo
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import pandas as pd
 
 app = Flask(__name__, static_url_path = '/static')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:admin@localhost:5432/app"
+db= SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class puntosModel(db.Model):
+    __tablename__ = 'Puntos'
+
+    manzana = db.Column(db.String, primary_key=True)
+    ciudad = db.Column(db.String())
+    personas = db.Column(db.String())
+    latitud = db.Column(db.Numeric())
+    longitud = db.Column(db.Numeric())
+    bajo = db.Column(db.Integer())
+    medio = db.Column(db.Integer())
+    alto = db.Column(db.Integer())
+
+    def __init__(self,manzana, ciudad, personas):
+        self.manzana = manzana
+        self.ciudad = ciudad
+        self.personas = personas
+
+    def __repr__(self):
+        return f"<Manzana {self.manzana}>"
 
 @app.route('/')
 def form():
@@ -10,7 +36,12 @@ def form():
     return render_template('Form.html',mensaje = mensaje)
 
 @app.route('/data', methods =['POST'])
+
 def data():
+    #Conversion de sql a dataframe
+    data = puntosModel.__tablename__
+    datapuntos = pd.read_sql_table(data,db.session.bind)
+    
     #Pide los datos del formulario
     ciudad = request.form['ciudad']
     puntos1 = int(request.form['puntos1'])
@@ -18,12 +49,14 @@ def data():
     puntos3 = int(request.form['puntos3'])
     # Hace sorteo con los datos del formulario
     
-    arreglo=mainSorteo(ciudad,puntos1,puntos2,puntos3)
+    arreglo=mainSorteo(datapuntos,ciudad,puntos1,puntos2,puntos3)
+    
     coordenadas = []
     for punto in arreglo:
-        coordenadas.append([punto[4],punto[5],punto[3],punto[6]])
+       coordenadas.append([punto[4],punto[5],punto[3],punto[6]])
     mensaje = "Puntos de " + ciudad
     return render_template('Form.html',mensaje = mensaje,arreglo=arreglo,coordenadas=coordenadas)
+    return 'ehh'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
--- sorteo.py
+++ sorteo.py
@@ -6,15 +6,18 @@ import geopandas as gpd
 import pandas as pd
 import random
 import reverse_geocoder as rg
-import pprint
 
-def pcd_leer_mgn():
-    fp = r"C:\Users\javie\Desktop\MGN2020_URB_AREA_CENSAL\MGN_URB_AREA_CENSAL.shp"
-    data = gpd.read_file(fp)
-    print(data.head())
-    f= data.plot()
 
-df = pd.read_excel(r"D:\Censo2018.xlsx", engine='openpyxl')
+
+
+
+#def pcd_leer_mgn():
+    #fp = r"C:\Users\javie\Desktop\MGN2020_URB_AREA_CENSAL\MGN_URB_AREA_CENSAL.shp"
+   # data = gpd.read_file(fp)
+  #  print(data.head())
+ #   f= data.plot()
+
+#df = pd.read_excel(r"D:\Censo2018.xlsx", engine='openpyxl')
 
 
 
@@ -36,9 +39,9 @@ def sortear(df_f,df_puntos,puntos, ciudad, nse, j):
         for z in df_f.iterrows():
             if acumulado >= persona[k]:            
                 #print (z)  
-                manzana = df_f['Manzana'].iloc[i]
-                lat = df_f['LATITUD'].iloc[i]
-                long = df_f['LONGITUD'].iloc[i]
+                manzana = df_f['manzana'].iloc[i]
+                lat = df_f['latitud'].iloc[i]
+                long = df_f['longitud'].iloc[i]
                 df_puntos.loc[j] = [ciudad, str(j) + " - " + nse, manzana , nse, lat, long]
                 break        
             acumulado = acumulado + df_f[nse].iloc[i]
@@ -51,71 +54,73 @@ def sortear(df_f,df_puntos,puntos, ciudad, nse, j):
     
    
 
-nse1 = "Bajo"
-nse2 = "Medio"
-nse3 = "Alto"
+nse1 = "bajo"
+nse2 = "medio"
+nse3 = "alto"
 
 
-# Filtro de Ciudad
-#df_f =   df[(df.Ciudad== 'Bogota') | (df.Ciudad== 'Soacha')]
+# Filtro de ciudad
+#df_f =   df[(df.ciudad== 'Bogota') | (df.ciudad== 'Soacha')]
 #puntos1 = 29
 #puntos2 =19
 #puntos3 = 4
 #ciudad = "Bogota y Soacha"
 
 
-#df_f =   df[(df.Ciudad== 'Medellin') | (df.Ciudad== 'Valle de Aburra')]
+#df_f =   df[(df.ciudad== 'Medellin') | (df.ciudad== 'Valle de Aburra')]
 #puntos1 = 18
 #puntos2 =12
 #puntos3 = 10
 #ciudad = "Medellin y Valle de Aburra"
 
-#df_f =   df[(df.Ciudad== 'Cali')]
+#df_f =   df[(df.ciudad== 'Cali')]
 #puntos1 = 22
 #puntos2 =12
 #puntos3 = 7
 #ciudad = "Cali"
 
-#df_f =   df[(df.Ciudad== 'Barranquilla') | (df.Ciudad== 'Soledad')]
+#df_f =   df[(df.ciudad== 'Barranquilla') | (df.ciudad== 'Soledad')]
 #puntos1 = 23
 #puntos2 =8
 #puntos3 = 9
 #ciudad = "Barranquilla y Soledad"
 
-#df_f =   df[(df.Ciudad== 'Bucaramanga') | (df.Ciudad== 'Floridablanca') | (df.Ciudad== 'Piedecuesta')]
+#df_f =   df[(df.ciudad== 'Bucaramanga') | (df.ciudad== 'Floridablanca') | (df.ciudad== 'Piedecuesta')]
 #puntos1 = 8
 #puntos2 =7
 #puntos3 = 5
 #ciudad = "Bucaramanga - Floridablanca - Piedecuesta"
 
-#df_f =   df[(df.Ciudad== 'Cartagena')]
+#df_f =   df[(df.ciudad== 'Cartagena')]
 #puntos1 = 28
 #puntos2 =9
 #puntos3 = 4
 #ciudad = "Cartagena"
 
 #Selector de ciudades
-def dfSelector(ciudad):
+def dfSelector(df,ciudad):
     if ciudad == "Bogota y Soacha":
-        df_f = df[(df.Ciudad== 'Bogota') | (df.Ciudad== 'Soacha')]
+        df_f = df[(df.ciudad== 'Bogota') | (df.ciudad== 'Soacha')]
     if ciudad == "Medellin y Valle de Aburra":
-        df_f = df[(df.Ciudad== 'Medellin') | (df.Ciudad== 'Valle de Aburra')]
+        df_f = df[(df.ciudad== 'Medellin') | (df.ciudad== 'Valle de Aburra')]
     if ciudad == "Barranquilla y Soledad":
-        df_f = df[(df.Ciudad== 'Barranquilla') | (df.Ciudad== 'Soledad')]
+        df_f = df[(df.ciudad== 'Barranquilla') | (df.ciudad== 'Soledad')]
     if ciudad == "Bucaramanga - Floridablanca - Piedecuesta":
-        df_f = df[(df.Ciudad== 'Bucaramanga') | (df.Ciudad== 'Floridablanca') | (df.Ciudad== 'Piedecuesta')]
+        df_f = df[(df.ciudad== 'Bucaramanga') | (df.ciudad== 'Floridablanca') | (df.ciudad== 'Piedecuesta')]
     if ciudad == "Cartagena":
-        df_f = df[(df.Ciudad== 'Cartagena')]
+        df_f = df[(df.ciudad== 'Cartagena')]
     if ciudad == "Cali":
-        df_f = df[(df.Ciudad== 'Cali')]
+        df_f = df[(df.ciudad== 'Cali')]
     if ciudad == "Pereira y Dosquebradas":
-        df_f = df[(df.Ciudad== 'Pereira') | (df.Ciudad== 'Dosquebradas')]
+        df_f = df[(df.ciudad== 'Pereira') | (df.ciudad== 'Dosquebradas')]
     return df_f
 
 #Establece los parametros del sorteo
-def mainSorteo(ciudad,puntos1,puntos2,puntos3):
-    df_puntos = pd.DataFrame(columns=['Ciudad', 'Punto', 'Manzana', 'Estrato' , 'Latitud', 'Longitud']) 
-    df_f =   dfSelector(ciudad)
+def mainSorteo(datapuntos,ciudad,puntos1,puntos2,puntos3):
+    df = datapuntos
+    
+    df_puntos = pd.DataFrame(columns=['ciudad', 'Punto', 'Manzana', 'Estrato' , 'Latitud', 'Longitud']) 
+    df_f =   dfSelector(df,ciudad)
     ciudad = ciudad
     # Numero de puntos muestrales en el estrado
     j = 1
@@ -144,3 +149,4 @@ def mainSorteo(ciudad,puntos1,puntos2,puntos3):
     else:
         df_puntos = df_puntos.iloc[0:0]
     return arreglo
+ 
